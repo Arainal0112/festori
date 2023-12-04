e3411cdc781d20a69eec38ef3c4396333ca3182c
anhar <fndedek@gmailcom>
2023-12-04 21:32:54 +0700

    make model and migration

---
+++ app/Models/Tiket.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Event;
+use App\Models\Transaksi;
+
+class Tiket extends Model
+{
+    use HasFactory;
+
+    protected $table = "tiket";
+    protected $fillable = [
+        'id',
+        'id_event',
+        'jumlah_tiket',
+        'harga_tiket',
+    ];
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class, 'id_event', 'id');
+    }
+
+    public function transaksi()
+    {
+        return $this->hasOne(Transaksi::class, 'id_transaksi', 'id');
+    }
+}
---
+++ app/Models/Transaksi.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Tiket;
+use App\Models\User;
+
+class Transaksi extends Model
+{
+    use HasFactory;
+
+    protected $table = "tiket";
+    protected $fillable = [
+        'id',
+        'id_tiket',
+        'id_user',
+        'total',
+    ];
+
+    public function tiket()
+    {
+        return $this->belongsTo(Tiket::class, 'id_tiket', 'id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user', 'id');
+    }
+}
+
+
